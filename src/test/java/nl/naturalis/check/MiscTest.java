24d19c840542dafc5d101d9e00faca57f830149c
Ayco Holleman <ayco.holleman@naturalis.nl>
2022-10-14 07:28:05 +0200

    split up unit tests for Misc

--- src/test/java/nl/naturalis/check/MiscTest.java
+++ src/test/java/nl/naturalis/check/MiscTest.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static nl.naturalis.check.Misc.toShortString;
 import static org.junit.Assert.assertEquals;
 
 public class MiscTest {
@@ -27,61 +28,91 @@ public class MiscTest {
 
   @Test
   public void toShortString00() {
-    assertEquals("null", Misc.toShortString(null, 10));
-    assertEquals("null", Misc.toShortString(null, 1));
-    assertEquals("OutputStream", Misc.toShortString(OutputStream.class, 15));
-    assertEquals("Outpu...", Misc.toShortString(OutputStream.class, 8));
-    assertEquals("OutputStream[][]", Misc.toShortString(OutputStream[][].class, 20));
-    assertEquals("O...", Misc.toShortString(OutputStream[][].class, 3));
-    assertEquals("hello", Misc.toShortString("hello", 10));
-    assertEquals("hello", Misc.toShortString("hello", 5));
-    assertEquals("h...", Misc.toShortString("hello", 4));
-    assertEquals("h...", Misc.toShortString("hello", 3));
-    assertEquals("h...", Misc.toShortString("hello", 2));
-    assertEquals("el...", Misc.toShortString("elephant", 5));
-    assertEquals("[]", Misc.toShortString(new int[0], 5, 8, 4));
-    assertEquals("[1]", Misc.toShortString(new int[] {1}, 10, 8, 4));
-    assertEquals("[1, 2]", Misc.toShortString(new int[] {1, 2}, 10, 8, 4));
-    assertEquals("[1, 2, 3]", Misc.toShortString(new int[] {1, 2, 3}, 10, 8, 4));
-    assertEquals("[1 (+2)]", Misc.toShortString(new int[] {1, 2, 3}, 20, 1, 4));
-    assertEquals("[ (+3)]", Misc.toShortString(new int[] {1, 2, 3}, 20, 0, 4));
+    assertEquals("null", toShortString(null, 10));
+    assertEquals("null", toShortString(null, 1));
+  }
+
+  @Test
+  public void toShortString02() {
+    assertEquals("OutputStream", toShortString(OutputStream.class, 15));
+    assertEquals("Outpu...", toShortString(OutputStream.class, 8));
+    assertEquals("OutputStream[][]", toShortString(OutputStream[][].class, 20));
+    assertEquals("O...", toShortString(OutputStream[][].class, 3));
+  }
+
+  @Test
+  public void toShortString03() {
+    assertEquals("hello", toShortString("hello", 10));
+    assertEquals("hello", toShortString("hello", 5));
+    assertEquals("h...", toShortString("hello", 4));
+    assertEquals("h...", toShortString("hello", 3));
+    assertEquals("h...", toShortString("hello", 2));
+    assertEquals("el...", toShortString("elephant", 5));
+  }
+
+  @Test
+  public void toShortString04() {
+    assertEquals("[]", toShortString(new int[0], 5, 8, 4));
+    assertEquals("[1]", toShortString(new int[] {1}, 10, 8, 4));
+    assertEquals("[1, 2]", toShortString(new int[] {1, 2}, 10, 8, 4));
+    assertEquals("[1, 2, 3]", toShortString(new int[] {1, 2, 3}, 10, 8, 4));
+    assertEquals("[1 (+2)]", toShortString(new int[] {1, 2, 3}, 20, 1, 4));
+    assertEquals("[ (+3)]", toShortString(new int[] {1, 2, 3}, 20, 0, 4));
     assertEquals("[1,...",
-        Misc.toShortString(new int[] {1, 2, 3, 4, 5}, 6, 8, 4));
+        toShortString(new int[] {1, 2, 3, 4, 5}, 6, 8, 4));
     assertEquals("[1, 2 (...",
-        Misc.toShortString(new int[] {1, 2, 3, 4, 5}, 10, 2, 4));
+        toShortString(new int[] {1, 2, 3, 4, 5}, 10, 2, 4));
     assertEquals("[1, 2 (+3)]",
-        Misc.toShortString(new int[] {1, 2, 3, 4, 5}, 13, 2, 4));
+        toShortString(new int[] {1, 2, 3, 4, 5}, 13, 2, 4));
+  }
 
-    assertEquals("[]", Misc.toShortString(new double[0], 5, 8, 4));
-    assertEquals("[1.0]", Misc.toShortString(new double[] {1}, 10, 8, 4));
-    assertEquals("[1.0, 2.0]", Misc.toShortString(new double[] {1, 2}, 10, 8, 4));
-    assertEquals("[1.0, 2...", Misc.toShortString(new double[] {1, 2, 3}, 10, 8, 4));
-    assertEquals("[1.0 (+2)]", Misc.toShortString(new double[] {1, 2, 3}, 20, 1, 4));
-    assertEquals("[ (+3)]", Misc.toShortString(new double[] {1, 2, 3}, 20, 0, 4));
+  @Test
+  public void toShortString05() {
+    assertEquals("[]", toShortString(new double[0], 5, 8, 4));
+    assertEquals("[1.0]", toShortString(new double[] {1}, 10, 8, 4));
+    assertEquals("[1.0, 2.0]", toShortString(new double[] {1, 2}, 10, 8, 4));
+    assertEquals("[1.0, 2...", toShortString(new double[] {1, 2, 3}, 10, 8, 4));
+    assertEquals("[1.0 (+2)]", toShortString(new double[] {1, 2, 3}, 20, 1, 4));
+    assertEquals("[ (+3)]", toShortString(new double[] {1, 2, 3}, 20, 0, 4));
     assertEquals("[1....",
-        Misc.toShortString(new double[] {1, 2, 3D, 4D, 5}, 6, 8, 4));
+        toShortString(new double[] {1, 2, 3D, 4D, 5}, 6, 8, 4));
     assertEquals("[1.0, 2...",
-        Misc.toShortString(new double[] {1, 2, 3D, 4D, 5}, 10, 2, 4));
+        toShortString(new double[] {1, 2, 3D, 4D, 5}, 10, 2, 4));
     assertEquals("[1.0, 2.0 ...",
-        Misc.toShortString(new double[] {1, 2, 3D, 4D, 5}, 13, 2, 4));
+        toShortString(new double[] {1, 2, 3D, 4D, 5}, 13, 2, 4));
+  }
 
-    assertEquals("[]", Misc.toShortString(new Double[0], 5, 8, 4));
+  @Test
+  public void toShortString06() {
+    assertEquals("[]", toShortString(new double[0], 5, 8, 4));
+    assertEquals("[1.0]", toShortString(new double[] {1}, 10, 8, 4));
+    assertEquals("[1.0, 2.0]", toShortString(new double[] {1, 2}, 10, 8, 4));
+    assertEquals("[1.0, 2...", toShortString(new double[] {1, 2, 3}, 10, 8, 4));
+    assertEquals("[1.0 (+2)]", toShortString(new double[] {1, 2, 3}, 20, 1, 4));
+    assertEquals("[ (+3)]", toShortString(new double[] {1, 2, 3}, 20, 0, 4));
+    assertEquals("[1....",
+        toShortString(new double[] {1, 2, 3D, 4D, 5}, 6, 8, 4));
+    assertEquals("[1.0, 2...",
+        toShortString(new double[] {1, 2, 3D, 4D, 5}, 10, 2, 4));
+    assertEquals("[1.0, 2.0 ...",
+        toShortString(new double[] {1, 2, 3D, 4D, 5}, 13, 2, 4));
+    assertEquals("[]", toShortString(new Double[0], 5, 8, 4));
     assertEquals("[1.0]",
-        Misc.toShortString(new Double[] {1D}, 10, 8, 4));
+        toShortString(new Double[] {1D}, 10, 8, 4));
     assertEquals("[1.0, 2.0]",
-        Misc.toShortString(new Double[] {1D, 2D}, 10, 8, 4));
+        toShortString(new Double[] {1D, 2D}, 10, 8, 4));
     assertEquals("[1.0, 2...",
-        Misc.toShortString(new Double[] {1D, 2D, 3D}, 10, 8, 4));
+        toShortString(new Double[] {1D, 2D, 3D}, 10, 8, 4));
     assertEquals("[ (+3)]",
-        Misc.toShortString(new Double[] {1D, 2D, 3D}, 20, 0, 4));
+        toShortString(new Double[] {1D, 2D, 3D}, 20, 0, 4));
     assertEquals("[ (+3)]",
-        Misc.toShortString(new Double[] {1D, 2D, 3D}, 20, 0, 4));
+        toShortString(new Double[] {1D, 2D, 3D}, 20, 0, 4));
     assertEquals("[1....",
-        Misc.toShortString(new Double[] {1D, 2D, 3D, 4D, 5D}, 6, 8, 4));
+        toShortString(new Double[] {1D, 2D, 3D, 4D, 5D}, 6, 8, 4));
     assertEquals("[1.0, 2...",
-        Misc.toShortString(new Double[] {1D, 2D, 3D, 4D, 5D}, 10, 2, 4));
+        toShortString(new Double[] {1D, 2D, 3D, 4D, 5D}, 10, 2, 4));
     assertEquals("[1.0, 2.0 ...",
-        Misc.toShortString(new Double[] {1D, 2D, 3D, 4D, 5D}, 13, 2, 4));
+        toShortString(new Double[] {1D, 2D, 3D, 4D, 5D}, 13, 2, 4));
 
   }
 
