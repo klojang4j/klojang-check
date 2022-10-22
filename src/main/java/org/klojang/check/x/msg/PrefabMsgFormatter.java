1d65e0c53c4f9242ecc1e6b9c6c69eac38ee3af1
Ayco Holleman <ayco.holleman@naturalis.nl>
2022-10-22 10:10:25 +0200

    made PrefabMsgFormatter package private

--- src/main/java/org/klojang/check/x/msg/PrefabMsgFormatter.java
+++ src/main/java/org/klojang/check/x/msg/PrefabMsgFormatter.java
@@ -3,4 +3,4 @@ package org.klojang.check.x.msg;
 import java.util.function.Function;
 
 @FunctionalInterface
-public interface PrefabMsgFormatter extends Function<MsgArgs, String> {}
+interface PrefabMsgFormatter extends Function<MsgArgs, String> {}
