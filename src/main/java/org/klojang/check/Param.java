b2921adf1a86c3892c29b72a99c4d6099c4bc4ef
Ayco Holleman <ayco.holleman@naturalis.nl>
2022-11-18 14:12:01 +0100

    klojang-collections unit tests copied into module

---
+++ src/main/java/org/klojang/check/Param.java
@@ -0,0 +1,125 @@
+package org.klojang.check;
+
+/**
+ * Ubiquitous parameter names.
+ */
+public final class Param {
+
+  /**
+   * "array"
+   */
+  public static final String ARRAY = "array";
+  /**
+   * "bean"
+   */
+  public static final String BEAN = "bean";
+  /**
+   * "capacity"
+   */
+  public static final String CAPACITY = "capacity";
+  /**
+   * "class"
+   */
+  public static final String CLAZZ = "class";
+  /**
+   * "collection"
+   */
+  public static final String COLLECTION = "collection";
+  /**
+   * "exception"
+   */
+  public static final String EXCEPTION = "exception";
+  /**
+   * "filter"
+   */
+  public static final String FILTER = "filter";
+  /**
+   * "from-index"
+   */
+  public static final String FROM_INDEX = "from-index";
+  /**
+   * "in"
+   */
+  public static final String IN = "in";
+  /**
+   * "index"
+   */
+  public static final String INDEX = "index";
+  /**
+   * "length"
+   */
+  public static final String LENGTH = "length";
+  /**
+   * "list"
+   */
+  public static final String LIST = "list";
+  /**
+   * "map"
+   */
+  public static final String MAP = "map";
+  /**
+   * "message"
+   */
+  public static final String MESSAGE = "message";
+  /**
+   * "message arguments"
+   */
+  public static final String MSG_ARGS = "message arguments";
+  /**
+   * "out"
+   */
+  public static final String OUT = "out";
+  /**
+   * "output"
+   */
+  public static final String OUTPUT = "output";
+  /**
+   * "path"
+   */
+  public static final String PATH = "path";
+  /**
+   * "property"
+   */
+  public static final String PROPERTY = "property";
+  /**
+   * "relative path"
+   */
+  public static final String RELATIVE_PATH = "relative path";
+  /**
+   * "set"
+   */
+  public static final String SET = "set";
+  /**
+   * "size"
+   */
+  public static final String SIZE = "size";
+  /**
+   * "test"
+   */
+  public static final String TEST = "test";
+  /**
+   * "to-index"
+   */
+  public static final String TO_INDEX = "to-index";
+  /**
+   * "type"
+   */
+  public static final String TYPE = "type";
+  /**
+   * "value"
+   */
+  public static final String VALUE = "value";
+  /**
+   * "values"
+   */
+  public static final String VALUES = "values";
+  /**
+   * "str"
+   */
+  public static final String STR = "str";
+
+  private Param() {
+    throw new UnsupportedOperationException();
+  }
+
+}
